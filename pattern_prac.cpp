9dd54b6d288d7255311fe2dbaac23b30686fc322
ANISH-SR <rawatanish08@gmail.com>
2023-11-17 18:04:48 +0530

    Added patter practise file
    Successfully added a pattern practise fil with a pyramidal pattern (Mirrored number pattern)

---
+++ pattern_prac.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int i = 1;
+    while(i<=n){
+        int space = n-i;
+        while(space){
+            cout << " ";
+            space = space - 1;
+        }
+        int j = 1;
+        int count = 1;
+        while(j<=i){
+            cout << count;
+            count = count +1;
+            j = j + 1;
+        }
+        int k = 1;
+        int start_cond = i-1;
+        while(start_cond){
+            cout << start_cond;
+            start_cond = start_cond - 1;
+        }
+        cout << endl;
+        i = i + 1;
+        }
+    }
